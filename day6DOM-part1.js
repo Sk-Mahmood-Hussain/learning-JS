3e94a4387b4bd1ff78ec9161ab47d7cd84d07ba9
Sk-Mahmood-Hussain <skmahmoodhussain07@gmail.com>
2024-09-01 00:43:48 +0530

    Complete my DOM PART 1

--- day6DOM-part1.js
+++ day6DOM-part1.js
@@ -49,9 +49,46 @@ console.dir(clasel);
 //Simillarly use # for IDs........ 
 
 
+//Child and parent node
+console.log(document.body.firstChild);            //will print text that is a hidden node
+console.log(document.querySelector("div").children);   //to access thhe chldren 
+
+// INNER TEXT >>>> returns the text conte tt of the element and all its children EX:-
+
+let div = document.querySelector("div");
+console.dir(div)
+
+// inner HTML
+// let inhtml = document.querySelector("html");
+// console.log(inhtml);
+//TEXT CONTENT>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//in console you can change the div just by div.innerText = "ABCD" 
+
+let h3ii = document.querySelector("h3");
+console.dir(h3ii);                  //In console write .... h3ii.innerText = "H3 Heading" //You Can vhange the text style by h3ii innerHTML = "<i>Hello </i>";
+
+//Q/1 ..>> Creat a h2 heading element with text -"Hello javaScript". Append "From Apna College Student" to This using Js
 
 
+let h2ii = document.querySelector("h2");
+h2ii.innerText = h2ii.innerText + "From Apna College"; //Concatinning
+console.dir(h2ii)
 
+// Q/2..CREAT A DIVS WITH COMMON CLASS NAME -"BOX". ACCESS THEN & ADD SOME UNIQUE TEXT TO EACH OF THEM .
+let dives = document.querySelectorAll(".box");
+console.log(dives);
+// dives[0].innerText = "kese hai re babuaa";
+// dives[1].innerText = "kese hai re babuaa";
+// dives[2].innerText = "kese hai re babuaa";
+
+// for(let t of dives){
+//     t.innerText = " i love you";
+// }
+// ind =1;
+// for(let k of dives){
+//     k.innerText=`yooo boys ${ind}`;
+//     ind++;
+// }
 
 
 
